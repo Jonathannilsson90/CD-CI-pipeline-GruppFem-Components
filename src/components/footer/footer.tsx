7956d659ee7c61b8495fcdbbcb6cf537a4c548cc
Una Paruma <parumauna@gmail.com>
2023-09-28 14:55:24 +0200

    #87

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -6,8 +6,18 @@ interface FooterProps {
 }
 
 const Footer = ({h1}: FooterProps) => {
-    return <footer className='header'>
+    return <footer className='Footer'>
         <h1>{h1}</h1>
+        <div className="star">
+            <ul>
+                <h3 className='link-title'><strong>Usefull links</strong></h3>
+                <div className='link-list'>
+                    <li>About</li>
+                    <li>Services</li>
+                    <li>Contacts</li>
+                </div>
+            </ul>
+        </div>
     </footer>;  
 }
 
