2db1a52e49f145880047ddc9a6166adad4165ec3
Yulia Gutorova <j.gutorova@gmail.com>
2023-09-30 21:57:12 +0200

    Skapa test för Button komponent #102

--- src/__test__/Button.test.tsx
+++ src/__test__/Button.test.tsx
@@ -1,11 +1,16 @@
 import React from "react";
-import {render} from "@testing-library/react"
-
+import {render, screen} from "@testing-library/react"
+import '@testing-library/jest-dom'
 import Button from "../components/button/Button"
 
 
 describe("Given:", () =>{
     test("Should render button component", () => {
         render(<Button label="Hello Jest Test!"/>)
+        const buttonElement = screen.getByRole("button");
+        console.log(buttonElement);  
+        expect(buttonElement).toBeInTheDocument();
+        expect(buttonElement).toHaveTextContent("Hello Jest Test!");
+        expect(buttonElement).toHaveClass("buttonOne");
     })
 })
