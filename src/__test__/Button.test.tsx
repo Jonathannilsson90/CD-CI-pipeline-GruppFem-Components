cfcb5fc1e4d51004f73d120afa6ec38500ef9bc8
Yulia Gutorova <j.gutorova@gmail.com>
2023-10-02 11:41:21 +0200

    skapa test f;r button komponent l'gga till mock function #102

--- src/__test__/Button.test.tsx
+++ src/__test__/Button.test.tsx
@@ -1,16 +1,26 @@
 import React from "react";
-import {render, screen} from "@testing-library/react"
+import {render, screen, fireEvent} from "@testing-library/react"
 import '@testing-library/jest-dom'
 import Button from "../components/button/Button"
 
 
 describe("Given:", () =>{
+
+    const handleClickMock = jest.fn();
+
+
     test("Should render button component", () => {
-        render(<Button label="Hello Jest Test!"/>)
+
+        render(<Button label="Hello Jest Test!" onClick={handleClickMock}/>)
+
         const buttonElement = screen.getByRole("button");
         console.log(buttonElement);  
+
         expect(buttonElement).toBeInTheDocument();
         expect(buttonElement).toHaveTextContent("Hello Jest Test!");
         expect(buttonElement).toHaveClass("buttonOne");
+
+        fireEvent.click(buttonElement);
+        expect(handleClickMock).toHaveBeenCalledTimes(1);
     })
 })
